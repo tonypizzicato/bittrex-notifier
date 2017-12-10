c996899a331f8228ba8f22fb07c26a48ff989215
tony.pizzicato <tony.grebnev@gmail.com>
2017-12-10 03:24:07 +0300

    store rates

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const axios = require('axios');
 const growl = require('growl');
 const argv = require('minimist')(process.argv.slice(2));
+const _ = require('lodash');
+const moment = require('moment');
 
-let last = 0;
+const store = {
+  markets: [],
+  values: {},
+};
 
+function getMarkets() {
+  return axios.get('https://bittrex.com/api/v1.1/public/getmarkets').then(response => response.data.result);
+}
 function getRate(market) {
-  const url = `http://bittrex.com/api/v1.1/public/getticker?market=${market}`;
+  const url = `http://bittrex.com/api/v1.1/public/getmarketsummary?market=${market}`;
 
-  return axios.get(url).then(response => response.data.result.Last);
+  return axios.get(url).then(response => response.data.result);
 }
 
 function fetch(market, diff, cb) {
@@ -15,28 +23,58 @@ function fetch(market, diff, cb) {
     .then(cb.bind(null, market, diff))
     .catch(error => console.warn(error));
 
-  setTimeout(fetch.bind(null, market, diff, cb), 10 * 1000);
+  setTimeout(fetch.bind(null, market, diff, cb), 20 * 1000);
 }
 
-function updateLastValue(market, diff, current) {
-  if (last && (current > last + diff || current < last - diff)) {
-    const currentStr = parseFloat(current).toFixed(2);
-    const lastStr = parseFloat(last).toFixed(2);
-    const emoji = current > last ? ':)' : ':(';
-    
-    growl(`${currentStr} vs ${lastStr} ${emoji}`, {
-      group: 'bittrex',
-      title: 'Bittrex',
-      subtitle: 'Price changed',
-      image: 'logo.png',
-      url: `https://bittrex.com/Market/Index?MarketName=${market}`,
-      sound: 'default',
-    });
+function processTick(market, diff, current) {
+  const timestr = _.get(current, '0.TimeStamp');
+  const value = _.get(current, '0.Last', 0);
+
+  if (!value || !timestr) {
+    return;
   }
-  last = current;
+
+  const time = moment(timestr).unix();
+
+  _.set(store, `values.${market}`, _.get(store, `values.${market}`, []).concat({ value, time }));
 }
 
 const market = argv['m'] || 'USDT-BTC';
 const diff = argv['d'] || 10;
 
-fetch(market, diff, updateLastValue);
+// get market tickers
+const markets = getMarkets();
+
+markets
+  .then(markets => markets.map(m => m.MarketName))
+  .then(markets => {
+    _.set(store, 'markets', markets);
+
+    return markets;
+  })
+  .then(markets => {
+    markets.forEach(m => fetch(m, diff, processTick));
+
+    return markets;
+  })
+  .catch(console.log);
+
+// function updateLastValue(market, diff, current) {
+//   const lastValue = last[market];
+
+//   if (lastValue && (current > lastValue + diff || current < lastValue - diff)) {
+//     const currentStr = parseFloat(current).toFixed(2);
+//     const lastStr = parseFloat(lastValue).toFixed(2);
+//     const emoji = current > lastValue ? ':)' : ':(';
+
+//     growl(`${currentStr} vs ${lastStr} ${emoji}`, {
+//       group: 'bittrex',
+//       title: 'Bittrex',
+//       subtitle: 'Price changed',
+//       image: 'logo.png',
+//       url: `https://bittrex.com/Market/Index?MarketName=${market}`,
+//       sound: 'default',
+//     });
+//   }
+//   last[market] = current;
+// }
